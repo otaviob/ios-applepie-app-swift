239083fd915bda1e594f72567949126b578712e2
otavio b <otaviob22@gmail.com>
2025-11-12 08:35:18 +1100

    UIButton Config

--- ios-applepie-app-swift/ViewController.swift
+++ ios-applepie-app-swift/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TreeImageView: UIImageView!
+    @IBOutlet weak var WordLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet var WordButton: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func WordButtonPress(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
