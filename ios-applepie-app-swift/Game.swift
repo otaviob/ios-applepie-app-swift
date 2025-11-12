2021ec75d0c358630d65cfbb7d644ea2608daffb
otavio b <otaviob22@gmail.com>
2025-11-12 16:02:56 +1100

    UpdateUI and NewRound

---
+++ ios-applepie-app-swift/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  ios-applepie-app-swift
+//
+//  Created by otavio brito on 12/11/2025.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectWord: Int
+}
--- ios-applepie-app-swift/ViewController.swift
+++ ios-applepie-app-swift/ViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+var listOfWords = ["swift", "csharp", "java", "kotlin", "objectivec", "javascript", "reative"]
+
+let incorrectMovesAllowed: Int = 7
+
+var totalWins = 0
+var totalLosses = 0
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var TreeImageView: UIImageView!
@@ -14,15 +22,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var ScoreLabel: UILabel!
     @IBOutlet var WordButton: [UIButton]!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    var activeGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        activeGame = Game(word: newWord, incorrectWord: incorrectMovesAllowed)
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        ScoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        TreeImageView.image = UIImage(named: "Tree_\(activeGame.incorrectWord)")
+    
+    }
 
     @IBAction func WordButtonPress(_ sender: UIButton) {
         sender.isEnabled = false
+        let letterString = sender.configuration!.title!
+        let letter = Character(letterString.lowercased())
     }
 }
 
